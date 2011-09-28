686769e9820d1a9d73114f8db7541e9d329d7b32
Oto Brglez <otobrglez@gmail.com>
2011-09-29 00:06:38 +0200

    Start project

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Aplikacija za primerjavo Avtomobilov (Implementacija algoritma MAUT)
+
+[mauto](http://mauto.opalab.com) je spletna aplikacija, ki omogoča izbiro najboljšega avtomobila z uporabo [metod MAUT](http://en.wikipedia.org/wiki/Multi-criteria_decision_analysis).
+
+# Avtor
+
+* [Oto Brglez](https://github.com/otobrglez)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mauto::Application.config.secret_token = '0ad490fb6084d90845cd769919962d40ec5471e7ea5473d1713a574a359e4cd0d1279b43789dfa609f0d425f17a609e4377419d535eb852195f59ead805b2754'
